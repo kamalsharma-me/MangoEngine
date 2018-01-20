269b9457db939efa53046c47cdb6f98bce9959c2
Kamal Sharma <kamal@skjapp.com>
2018-01-21 02:44:54 +0530

    Core Engine Added
    Core engine directory created with json.

---
+++ Libgdx/MangoEngine/src/main/java/com/mango/engine/Engine.java
@@ -0,0 +1,30 @@
+package com.mango.engine;
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Screen;
+
+public class Engine extends Game implements Screen
+{
+    @Override
+    public void create()
+    {
+
+    }
+
+    @Override
+    public void show()
+    {
+
+    }
+
+    @Override
+    public void render(float delta)
+    {
+
+    }
+
+    @Override
+    public void hide()
+    {
+
+    }
+}
